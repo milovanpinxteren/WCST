293a697561da470c906817916cd44ff49fbd5ced
milovanpinxteren <milo.van.pinxteren@gmail.com>
2023-02-06 18:04:48 +0100

    end of day: added todos

--- scoutingtool/classes/FilterPlayers.py
+++ scoutingtool/classes/FilterPlayers.py
@@ -13,19 +13,24 @@ class FilterPlayers():
     def filter_players(self, criterea_list):
         queryset = GeneralPlayerInfo.objects.all()
         filtered_id_list = GeneralPlayerInfo.objects.all().values_list('id', flat=True)
+
         for criterium in criterea_list:
-            if criterium['field'] == 'player_position':
-                if criterium['value'] == '0':
-                    queryset = queryset
-                else:
-                    queryset = queryset.filter(position_id=criterium['value'])
-            elif criterium['field'] == 'player_age_min':
-                queryset = queryset.filter(player_age__gte=criterium['value'])
-            elif criterium['field'] == 'player_age_max':
-                queryset = queryset.filter(player_age__lte=criterium['value'])
-            elif criterium['field'] in self.shooting_fields:
-                filtered_id_list = self.shootingfilter.filter_players(criterium)
+            if len(queryset) >= 10:
+                if criterium['field'] == 'player_position':
+                    if criterium['value'] == '0':
+                        queryset = queryset
+                    else:
+                        queryset = queryset.filter(position_id=criterium['value'])
+                elif criterium['field'] == 'player_age_min':
+                    queryset = queryset.filter(player_age__gte=criterium['value'])
+                elif criterium['field'] == 'player_age_max':
+                    queryset = queryset.filter(player_age__lte=criterium['value'])
+                elif criterium['field'] in self.shooting_fields:
+                    filtered_id_list = self.shootingfilter.filter_players(criterium)
             #TODO: do this for passing, possession etc
+            elif len(queryset) < 10:
+                print(queryset)
+                #TODO: get more players/loosen constraints
 
             queryset = queryset.filter(id__in=filtered_id_list)
         return queryset
